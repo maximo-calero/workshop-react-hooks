cf5f55449ab209946fd2379ce1b4d1f490a1add1
Maximo Calero Sanchez (github) <maximo.calero@outlook.com>
2021-05-15 09:53:24 +0200

    Component MovieCard created

--- src/components/result-search/ResultSearch.tsx
+++ src/components/result-search/ResultSearch.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ResultSearch.scss';
 import { Movie } from '../../common/interfaces/movie';
-import { truncateLongString } from '../../common/utils/utils'
+import MovieCard from './components/movie-card/MovieCard';
 
 interface Props {
     movies: Movie[];
@@ -26,25 +26,7 @@ class ResultSearch extends React.Component<Props,{}> {
             <div className='result-search'>
                 <div className='result-search__items'>
                 {this.props.movies && this.props.movies.length > 0 &&
-                    this.props.movies.map(movie => {
-                        return (
-                            <div 
-                                key={movie.id} 
-                                className='result-search__items__item'
-                            >
-                                <div 
-                                    className="result-search__items__item__image"
-                                    style={{
-                                        backgroundImage: `url(${movie.imageUrl})`,
-                                    }}
-                                ></div>
-                                <h2 title={movie.title} className="result-search__items__item__title">{truncateLongString(movie.title).truncate(30, 1)}</h2>
-                                <p className="result-search__items__item__overview">{truncateLongString(movie.overview).truncate(150, 1)}</p>
-                                <span className="result-search__items__item__release-date">{movie.releaseDate.format('DD/MM/YYYY')}</span>
-                                <span className="result-search__items__item__rate">{movie.rate}</span>
-                            </div>
-                        );
-                    })
+                    this.props.movies.map(movie => (<MovieCard movie={movie} />))
                 }
                 </div>
             </div>        
